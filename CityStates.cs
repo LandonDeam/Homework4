01003deab6d2697ced8874760f349d0327b7c4c9
Landon Deam <landondeam@gmail.com>
2024-04-14 23:41:34 -0400

    setting up solution

---
+++ CityStates.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace Cities
+{
+    public class CityStates
+    {
+        public static void Solution(string File = "CityStates.txt")
+        {
+            try
+            {
+                
+            }
+            catch (Exception) {}
+        }
+    }
+}
