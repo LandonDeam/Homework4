54c6aa9437335576f5d755cc001cca0caeddaf86
Landon Deam <landondeam@gmail.com>
2024-04-14 22:01:31 -0400

    starter class

---
+++ CommonCity.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System;
+
+public class CommonCity
+{
+    private static Dictionary<string, List<string>> cities = new Dictionary<string, List<string>>();
+
+    public static void add(string city, string state) {
+        if (cities[city] is not null && !cities[city].Contains(state)) 
+        {
+            cities[city].Add(state);
+        }
+    }
+
+    public static List<string> getCommon(string state1, string state2)
+    {
+        List<string> list = new List<string>();
+        foreach (string city in cities.Keys)
+        {
+            if (cities[city] is not null &&
+                cities[city].Contains(state1) &&
+                cities[city].Contains(state2)
+                )
+            {
+                list.Add(city);
+            }
+        }
+        return list;
+    }
+}
